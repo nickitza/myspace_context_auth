d3863dff36f9ee7d8661f35cdc41c3a90e0de6e7
Nicki Nixon <nickitza@gmail.com>
2019-06-17 17:52:25 -0600

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,72 @@
-import React from 'react'
+import React from "react";
+import { AuthConsumer } from "../providers/AuthProvider";
+import { Menu } from "semantic-ui-react";
+import { Link, withRouter } from "react-router-dom";
 
 class Navbar extends React.Component {
+  rightNavItems = () => {
+    const {
+      auth: { user, handleLogout },
+      location
+    } = this.props;
 
-  render(){
-    return(
-      <>
-      </>
-    )
-  } 
+    if (user) {
+      return (
+        <Menu.Menu postion="right">
+          <Menu.Item
+            name="logout"
+            onClick={() => handleLogout(this.props.history)}
+          />
+        </Menu.Menu>
+      );
+    } else {
+      return (
+        <Menu.Menu position="right">
+          <Link to="/login">
+            <Menu.Item
+              id="login"
+              name="login"
+              active={location.pathname === "/login"}
+            />
+          </Link>
+          <Link to="/register">
+            <Menu.Item
+              id="register"
+              name="register"
+              active={location.pathname === "/register"}
+            />
+          </Link>
+        </Menu.Menu>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Link to="/">
+            <Menu.Item
+              id="home"
+              name="home"
+              active={this.props.location.pathname === "/"}
+            />
+          </Link>
+          {this.rightNavItems()}
+        </Menu>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export class ConnectedNavbar extends React.Component {
+  render() {
+    return (
+      <AuthConsumer>
+        {auth => <Navbar {...this.props} auth={auth} />}
+      </AuthConsumer>
+    );
+  }
+}
+
+export default withRouter(ConnectedNavbar);
